7bca1530865fb77380292a21c218123cf93b7d19
remyasantos <remyasantos@gmail.com>
2026-04-08 13:52:53 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
-      <body style={{ margin: 0, background: "#0a0a0f" }}>{children}</body>
+      <body style={{ margin: 0 }}>{children}</body>
     </html>
   )
 }
